e7d6cff4a260213ca72cea881be403f7ff0c33de
sirirakNamsuk <sirirak_21@hotmail.com>
2017-11-19 15:05:38 +0700

    Initial commit

---
+++ src/oop_lab/s357411110041/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.s357411110041.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world, I'am RAK");
+
+    
+}// main
+
+}// class
