b682f02a52b48bde01f6a4e0a4ff2cbbfeab863b
sirirak Namsuk <sirirak_21@hotmail.com>
2018-01-30 09:49:57 +0700

    Student OOP8

--- src/OOP_Lab8/PersonInfo.java
+++ src/OOP_Lab8/PersonInfo.java
@@ -1,4 +1,16 @@
 package OOP_Lab8;
 
 public class PersonInfo {
-}
+    public static void main(String[] args) {
+        Student student = new Student("357411110041",
+                "Girl Saiyai",
+                "Male",
+                22,
+                "357411110041",
+                "IS: Management Technology",);
+        System.out.println(student.toString());
+
+
+
+    }//main
+}//class
