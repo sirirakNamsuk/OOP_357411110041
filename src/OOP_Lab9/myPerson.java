dbfef04da7d85e78fe1520280a544de50fef994d
sirirak Namsuk <sirirak_21@hotmail.com>
2018-02-06 11:03:22 +0700

    myPerson

--- src/OOP_Lab9/myPerson.java
+++ src/OOP_Lab9/myPerson.java
@@ -7,7 +7,13 @@ public class myPerson {
         Job job=new Job("sales",25000);
         //person
         Person Person=new Person("12345","Rak",address,job);
+
         System.out.print(Person.toString());
+
+        Person.getJob().setSalary(30000);
+
+        System.out.println(Person.getJob().getSalary());
+        System.out.println(Person.getAddress().getPostCode());
 
     }
 }
